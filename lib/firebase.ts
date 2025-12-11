9ff1e03c3ba39f33f7852c9220be2b772071d632
Efim2003 <fima.rubin@gmail.com>
2025-12-11 22:41:53 +0200

    app-hosting

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,25 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAhXYMmHAX6YVj_O2FiebTLvug61QdeU1I",
-  authDomain: "gen-lang-client-0783951303.firebaseapp.com",
-  projectId: "gen-lang-client-0783951303",
-  storageBucket: "gen-lang-client-0783951303.firebasestorage.app",
-  messagingSenderId: "768806664656",
-  appId: "1:768806664656:web:ade93af07af7519ab3a1d5",
-  measurementId: "G-8TXTHG7725"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp();
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
 export {db};
